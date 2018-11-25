4cb233025e88f8068b1b3d15a9e6afe81961968c
wrk <wrks@mac.com>
2018-11-25 14:46:09 +0100

    final app

---
+++ src/components/layout/Search.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import SelectField from 'material-ui/SelectField';
+import MenuItem from 'material-ui/MenuItem';
+import axios from 'axios';
+
+import ImageResults from './ImageResults';
+
+class Search extends Component {
+  state = {
+    searchText: '',
+    amount: 5,
+    apiUrl: 'https://pixabay.com/api',
+    apiKey: '',
+    images: []
+  };
+
+  onTextChange = e => {
+    const val = e.target.value;
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === '') {
+        this.setState({ images: [] });
+      } else {
+        axios
+          .get(
+            `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+              this.state.searchText
+            }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+          )
+          .then(res => this.setState({ images: res.data.hits }))
+          .catch(err => console.log(err));
+      }
+    });
+  };
+
+  onAmountChnage = (e, index, value) => this.setState({ amount: value });
+
+  render() {
+    console.log(this.state);
+    return (
+      <div>
+        <TextField
+          name="searchText"
+          value={this.state.searchText}
+          onChange={this.onTextChange}
+          floatingLabelText="Search For mages"
+          fullWidth={true}
+        />
+        <br />
+
+        <SelectField
+          name="amount"
+          floatingLabelText="Amount"
+          value={this.state.amount}
+          onChange={this.onAmountChnage}
+        >
+          <MenuItem value={5} primaryText="5" />
+          <MenuItem value={10} primaryText="10" />
+          <MenuItem value={15} primaryText="15" />
+          <MenuItem value={30} primaryText="30" />
+          <MenuItem value={50} primaryText="50" />
+        </SelectField>
+        <br />
+
+        {this.state.images.length > 0 ? (
+          <ImageResults images={this.state.images} />
+        ) : null}
+      </div>
+    );
+  }
+}
+
+export default Search;
